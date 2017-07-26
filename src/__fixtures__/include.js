2b059e63b83527e2496a9950c066e125c1d7327a
AndersDJohnson <AndersDJohnson@users.noreply.github.com>
2017-07-26 18:35:38 -0500

    test: modules from file

--- src/__fixtures__/include.js
+++ src/__fixtures__/include.js
@@ -1,2 +1,4 @@
-export default /node_modules\/(babel-loader|callsites|enhanced-resolve|esprima|find-cache-dir|find-up|jest|jest-cli|loader-runner|loader-utils|locate-path|make-dir|p-limit|p-locate|p-map|path-exists|pkg-dir|request|strip-indent|uglifyjs-webpack-plugin|webpack)\/.*/
+import modules from './modules'
 
+
+export default new RegExp(`node_modules\/(${modules})\/.*`)
---
+++ src/__fixtures__/modules.js
@@ -0,0 +1,5 @@
+import fs from 'fs'
+import path from 'path'
+
+
+export default fs.readFileSync(path.join(__dirname, 'modules.txt'), 'utf8').trim()
