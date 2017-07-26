24ddcc74f76a0f39fca736cc1c2cb7235ab080ce
AndersDJohnson <AndersDJohnson@users.noreply.github.com>
2017-07-26 00:34:54 -0500

    test: add for require package __non_webpack_require__

---
+++ src/require-package.test.js
@@ -0,0 +1,9 @@
+import requirePackage from './require-package'
+
+describe('require-package', () => {
+  it('works for __non_webpack_require__', () => {
+    global.__non_webpack_require__ = jest.fn()
+    requirePackage('test')
+    expect(global.__non_webpack_require__).toHaveBeenCalledWith('test/package.json')
+  })
+})
