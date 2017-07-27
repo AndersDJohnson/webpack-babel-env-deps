374c8480481f8fad5dd738adac58886dd6ff127b
AndersDJohnson <AndersDJohnson@users.noreply.github.com>
2017-07-27 18:14:47 -0500

    refactor: separate non-webpack-require

---
+++ src/non-webpack-require.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line camelcase, no-undef
+export default path => typeof __non_webpack_require__ !== 'undefined' ? __non_webpack_require__(path) : require(path)
--- src/require-package.js
+++ src/require-package.js
@@ -1,8 +1,8 @@
 import path from 'path'
+import nonWebpackRequire from './non-webpack-require'
 
 export default function requirePackage (pkgRoot) {
   const pkgPath = path.join(pkgRoot, 'package.json')
 
-  // eslint-disable-next-line camelcase, no-undef
-  return typeof __non_webpack_require__ !== 'undefined' ? __non_webpack_require__(pkgPath) : require(pkgPath)
+  return nonWebpackRequire(pkgPath)
 }
