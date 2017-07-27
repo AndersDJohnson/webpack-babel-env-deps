83ee5e9e2ae28791f8dada6da9ba29ba38e3f754
AndersDJohnson <AndersDJohnson@users.noreply.github.com>
2017-07-27 04:51:35 -0500

    test: add for node modules regex

---
+++ src/node-modules-regex.test.js
@@ -0,0 +1,25 @@
+/* eslint-env jest */
+
+import { include, exclude } from './node-modules-regex'
+
+describe('node-modules-regex', () => {
+  describe('include', () => {
+    it('matches included', () => {
+      expect('node_modules/foo/index.js').toMatch(include(['foo']))
+    })
+
+    it('does not match non-included', () => {
+      expect('node_modules/bar/index.js').not.toMatch(include(['foo']))
+    })
+  })
+
+  describe('exclude', () => {
+    it('does not match excluded', () => {
+      expect('node_modules/foo/index.js').not.toMatch(exclude(['foo']))
+    })
+
+    it('matches non-excluded', () => {
+      expect('node_modules/bar/index.js').not.toMatch(exclude(['foo']))
+    })
+  })
+})
